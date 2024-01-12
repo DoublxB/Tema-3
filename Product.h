e41473a954af910d015c511c320f67f78cf9ddab
DoublxB <122833282+DoublxB@users.noreply.github.com>
2024-01-12 04:32:24 +0200

    Create Product.h

---
+++ Product.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+class Product {
+public:
+    Product(const std::string& name, double price);
+
+    std::string getName() const;
+    double getPrice() const;
+
+private:
+    std::string name;
+    double price;
+};
